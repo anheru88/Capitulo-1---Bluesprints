08c483498fa89e8343fefe5541b8c546721319ad
Angel Jimenez <ajimenez@digitalcaribe.com.co>
2014-03-29 17:43:54 -0500

    Agregada la ruta del post, para el formulario.

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,42 @@ Route::get('/', function()
 {
 	return View::make('form');
 });
+
+Route::post('/',function(){
+//We first define the Form validation rule(s)
+	$rules = array(
+		'link' => 'required|url'
+		);
+//Then we run the form validation
+	$validation = Validator::make(Input::all(),$rules);
+//If validation fails, we return to the main page with an error info
+	if($validation->fails()) {
+		return Redirect::to('/')
+		->withInput()
+		->withErrors($validation);
+	} else {
+//Now let's check if we already have the link in our database. If so, we get the first result
+		$link = Link::where('url','=',Input::get('link'))->first();
+//If we have the URL saved in our database already, we provide that information back to view.
+		if($link) {
+			return Redirect::to('/')
+			->withInput()
+			->with('link',$link->hash);
+//Else we create a new unique URL
+		} else {
+//First we create a new unique Hash
+			do {
+				$newHash = Str::random(6);
+			} while(Link::where('hash','=',$newHash)->count() > 0);
+//Now we create a new database record
+			Link::create(array(
+				'url' => Input::get('link'),
+				'hash' => $newHash
+				));
+//And then we return the new shortened URL info to our action
+			return Redirect::to('/')
+			->withInput()
+			->with('link',$newHash);
+		}
+	}
+});
