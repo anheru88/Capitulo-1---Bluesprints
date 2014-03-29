9333d03037392579542a44e57aca439eed4938bb
Angel Jimenez <ajimenez@digitalcaribe.com.co>
2014-03-29 17:45:39 -0500

    Agregados mensajes de éxito y fracaso.

--- app/views/form.blade.php
+++ app/views/form.blade.php
@@ -6,12 +6,21 @@
 </head>
 <body>
 	<div id="container">
+		@if(Session::has('errors'))
+		<h3 class="error">{{$errors->first('link')}}</h3>
+		@endif
+		@if(Session::has('link'))
+		<h3 class="success">
+			{{Html::link(Session::get('link'),'Click here for your
+			shortened URL')}}
+		</h3>
+		@endif
 		<h2>Uber-Shortener</h2>
 		{{Form::open(array('url'=>'/','method'=>'post'))}}
 		{{Form::text('link',Input::old('link'),
-		array('placeholder'=>
-		'Insert your URL here and press enter!'))}}
-		{{Form::close()}}
-	</div>
-</body>
-</html>
+			array('placeholder'=>
+			'Insert your URL here and press enter!'))}}
+			{{Form::close()}}
+		</div>
+	</body>
+	</html>
