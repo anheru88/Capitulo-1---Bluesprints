94cf487c5f4da218035cc1a530d7d6384a25da09
Angel Jimenez <ajimenez@digitalcaribe.com.co>
2014-03-29 17:47:43 -0500

    Agregada la ruta del hash y Redirección a la pagina web.

--- app/routes.php
+++ app/routes.php
@@ -54,3 +54,17 @@ Route::post('/',function(){
 		}
 	}
 });
+
+Route::get('{hash}',function($hash) {
+//First we check if the hash is from a URL from our database
+	$link = Link::where('hash','=',$hash)
+	->first();
+//If found, we redirect to the URL
+	if($link) {
+		return Redirect::to($link->url);
+//If not found, we redirect to index page with error message
+	} else {
+		return Redirect::to('/')
+		->with('message','Invalid Link');
+	}
+})->where('hash', '[0-9a-zA-Z]{6}');
